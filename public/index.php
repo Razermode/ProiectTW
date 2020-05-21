81a2402b552e7b4eb89c656215f99cd414322445
Razermode <57268043+Razermode@users.noreply.github.com>
2020-05-21 16:57:01 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,10 @@
 <?php
 
 use Api\Action\FactoryDispatcher;
+use Api\Action\TaskDispatcher;
+use Api\Action\UserDispatcher;
 use Api\Exception\ExceptionInterface as ApiException;
+use Api\Exception\NotFound as NotFoundException;
 use Api\Request;
 use Api\Response;
 
@@ -18,13 +21,21 @@ include __DIR__ . '/../src/api/autoloader.php';
 $requestURI = strtolower(trim($_SERVER['REQUEST_URI']," /"));
 $params = explode('/', $requestURI);
 try {
+    
     $request = Request::getInstance();
     $response = Response::getInstance();
-    
-    FactoryDispatcher::select($params[0])
-        ->dispatch($request, $response)
-        ->execute()
-    ;
+    switch ($params[0]) {
+        case 'task':
+            $app = new TaskDispatcher();
+            break;
+        case 'user':
+            $app = new UserDispatcher();
+            break;
+        default:
+            throw new NotFoundException("Unknown [{$params[0]}] entity", 404);
+    }
+    $app->dispatch($request, $response);
+
     
     $httpResponseCode = $response->getHttpResponseCode();
     $data = $response->getData();
